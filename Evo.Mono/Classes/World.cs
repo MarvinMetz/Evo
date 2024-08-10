9b4ee5f8f83ce9f52f21d36010bcc35253d43648
Marvin <info@marvinmetz.de>
2024-08-10 14:20:05 +0200

    feat(logic): add grid to world

--- Evo.Mono/Classes/World.cs
+++ Evo.Mono/Classes/World.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using Evo.Mono.Classes.PathFinding;
 using Microsoft.Xna.Framework;
 
 namespace Evo.Mono.Classes;
 
 public class World
 {
-    public int Size { get; set; }
+    public int WorldSize { get; init; }
+
+    private Grid _grid;
+    private int _gridResolution;
 
     public ICollection<Entity> Entities { get; set; } = [];
+
+
+    public World(int worldSize, int gridResolution)
+    {
+        if (WorldSize % _gridResolution != 0)
+            throw new ArgumentException("Grid resolution must divide world size without remainder.",
+                nameof(gridResolution));
+
+        WorldSize = worldSize;
+        _gridResolution = gridResolution;
+
+        var nodeAmount = WorldSize / _gridResolution;
+        nodeAmount += 2; // Border
+
+        _grid = new Grid(nodeAmount, nodeAmount);
+        _grid.SetBorderNodesWalkable(false);
+    }
 
     public bool IsPositionOnWorld(float offset, params Vector2[] positions)
     {
         foreach (var position in positions)
-            if (position.X < 0 + offset || position.X > Size || position.Y < 0 + offset ||
-                position.Y > Size)
+            if (position.X < 0 + offset || position.X > WorldSize || position.Y < 0 + offset ||
+                position.Y > WorldSize)
             {
                 return false;
             }
